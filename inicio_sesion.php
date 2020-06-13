0b8ff1cb0c5baf1b61070d69b2f489dd2b903ec6
unknown <fer.dog9714@gmail.com>
2020-06-13 15:36:44 -0500

    Pagina de inicio de sesion modificada

--- inicio_sesion.php
+++ inicio_sesion.php
@@ -68,56 +68,30 @@ $sql_objeto->cerrar_mysqli();
 
 
 
-
-    <div class="container mt-5 py-3">
-      <div class="row">
-        <div class="col-5">
-          <header class="bg-info text-white">
-            <div class="ml-5 mb-5" >
-              <img src="imagenes/logo.png" width="350"
-              height="301">
-            </div>
-          </header>
-        </div>
-        <div class="col-7 align-items-center mt-5 py-5">
-          <div class="container-fluid">             
-              <div class="login py-5 ">
-          
-                  <div class="col-md-4 col-lg-8 mx-auto">
-                        <h3 class="login-heading mb-4 text-sm-center">INICIO DE SESION</h3>
-
-                        
-                      <form method ="post">
-
-                          <div class="form-label-group py-3 ">
-                              <?php
-                                  echo $error;
-                              ?>
-                          </div>
-
-                          <div class="form-label-group py-3 ">
-                            <input type="email" name = "email" id="inputEmail" class="form-control" placeholder="Correo Electronico">
-                            
-                          </div>
-          
-                          <div class="form-label-group py-3 mb-3">
-                            <input type="password" id="inputPassword" name = "password" class="form-control" placeholder="Contraseña" >
-                            
-                          </div>
-                          
-                              
-                          <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit">Iniciar Sesion</button>
-                          <div class="form-group"> <!-- State Button -->               
-                        </div>
-                          <div class="text-center">
-                            <a class="small" href="#" >¿Olvidaste tu contraseña?</a></div>
-                        </form>
-                    </div>
-                </div>
+    <section class="container">
+      <div class="col-md-4 col-lg-8 mx-auto">
+        <h3 class="login-heading mb-4 text-sm-center pt-">INICIO DE SESION</h3>
+        <form method ="post" class="ml-auto mr-auto">
+          <div class="form-label-group py-3">
+            <?php
+              echo $error;
+            ?>
           </div>
-        </div>
+          <div class="form-label-group py-3 ">
+            <input type="email" name = "email" id="inputEmail" class="form-control" placeholder="Correo Electronico">
+          </div>          
+          <div class="form-label-group py-3 mb-3">
+            <input type="password" id="inputPassword" name = "password" class="form-control" placeholder="Contraseña" >                  
+          </div>                    
+          <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit">Iniciar Sesion</button>
+          <div class="form-group"> <!-- State Button -->               
+          </div>
+          <div class="text-center">
+            <a class="small" href="#" >¿Olvidaste tu contraseña?</a>
+          </div>
+        </form>
       </div>
-    </div>
+    </section>
 
 
   
